a9ec8e6998b94be9f935b5aff6a7d638417bbd03
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-25 10:10:28 +0200

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter">
+      0
+    </div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
 </script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
